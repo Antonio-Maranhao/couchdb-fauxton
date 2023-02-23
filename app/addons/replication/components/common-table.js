44f20c3ae723e454d1e1eccf20919bb5d542a092
Antonio Maranhao <antonio.maranhao@gmail.com>
2023-02-23 14:06:03 -0500

    Fix: replication list action buttons

--- app/addons/replication/components/common-table.js
+++ app/addons/replication/components/common-table.js
@@ -15,7 +15,11 @@ import {Button, Table, Tooltip, OverlayTrigger} from "react-bootstrap";
 import moment from 'moment';
 import {ErrorModal} from './modals';
 import {removeCredentialsFromUrl} from '../api';
+import Components from "../../components/react-components";
 import Helpers from '../../../helpers';
+import FauxtonAPI from "../../../core/api";
+
+const { ToolbarButton } = Components;
 
 const getDbNameFromUrl = (urlObj, root) => {
   try {
@@ -127,43 +131,30 @@ const RowActions = ({onlyDeleteAction, _id, url, deleteDocs}) => {
   const actions = [];
   if (!onlyDeleteAction) {
     actions.push(
-      <li className="replication__row-list" key={1}>
-        <a
-          href={`#replication/id/${encodeURIComponent(_id)}`}
-          className="replication__row-btn fonticon-wrench replication__row-btn--no-left-pad"
-          title={'Edit replication'}
-          data-bypass="true"
-        >
-        </a>
-      </li>
+      <ToolbarButton icon="fonticon-wrench"
+        aria-label={`Edit replication`}
+        key={1}
+        onClick={() => FauxtonAPI.navigate(`#replication/id/${encodeURIComponent(_id)}`)} />
     );
     actions.push(
-      <li className="replication__row-list" key={2}>
-        <a
-          className="replication__row-btn fonticon-document"
-          title={'Edit replication document'}
-          href={url}
-          data-bypass="true"
-        >
-        </a>
-      </li>
+      <ToolbarButton icon="fonticon-document"
+        aria-label={`Edit replication document ${_id}`}
+        key={2}
+        onClick={() => FauxtonAPI.navigate(url)} />
     );
   }
 
   actions.push(
-    <li className="replication__row-list" key={3}>
-      <a
-        className={`replication__row-btn fonticon-trash ${onlyDeleteAction ? 'replication__row-btn--no-left-pad' : ''} `}
-        title={`Delete ${onlyDeleteAction ? 'job' : 'document'} ${_id}`}
-        onClick={() => deleteDocs(_id)}>
-      </a>
-    </li>
+    <ToolbarButton icon="fonticon-trash"
+      aria-label={`Delete ${onlyDeleteAction ? 'job' : 'document'} ${_id}`}
+      key={3}
+      onClick={() => deleteDocs(_id)} />
   );
 
   return (
-    <ul className="replication__row-actions-list">
+    <div className="replication__actions">
       {actions}
-    </ul>
+    </div>
   );
 };
 
