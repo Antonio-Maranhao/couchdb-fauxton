e119f4ba59e8d5d1b99e8ab49380c08ab93260db
Antonio Maranhao <antonio.maranhao@gmail.com>
2023-03-06 11:50:58 -0500

    Fix: missing button title

--- app/addons/documents/doc-editor/components/PanelButton.js
+++ app/addons/documents/doc-editor/components/PanelButton.js
@@ -35,6 +35,7 @@ export default class PanelButton extends React.Component {
       <div className="panel-section">
         <Button variant='cf-secondary'
           onClick={this.props.onClick}
+          title={this.props.title}
           className={this.props.className}
           disabled={this.props.disabled} >
           <i className={this.props.iconClass}></i>
