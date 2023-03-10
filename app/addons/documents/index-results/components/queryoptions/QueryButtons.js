a09e2a4fc0f4587008b5eb4c82b4bcd7eb4dc7ad
Jordan Fry <94444185+fryjordan@users.noreply.github.com>
2023-03-10 13:14:02 +0000

    fix margins on query modal buttons (#55)

--- app/addons/documents/index-results/components/queryoptions/QueryButtons.js
+++ app/addons/documents/index-results/components/queryoptions/QueryButtons.js
@@ -26,7 +26,7 @@ export default class QueryButtons extends React.Component {
 
   render () {
     return (
-      <div className="row m-2 mt-0 text-end">
+      <div className="row mx-2 mb-3 mt-0 text-end">
         <div id="button-options" className="col">
           <Button type="submit" variant="cf-secondary">Run Query</Button>
           <a onClick={this.hideTray.bind(this)} className="btn btn-link">Cancel</a>
