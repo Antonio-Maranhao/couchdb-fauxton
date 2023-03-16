9548cb2a3a7312d89275c7fb476941752fcf8866
Jordan Fry <Jordan.Fry@ibm.com>
2023-03-16 09:40:30 +0000

    fix e2e tests: viewCreateBadView file

--- app/addons/documents/tests/nightwatch/viewCreateBadView.js
+++ app/addons/documents/tests/nightwatch/viewCreateBadView.js
@@ -23,7 +23,7 @@ module.exports = {
       .populateDatabase(newDatabaseName)
       .url(baseUrl + '/#/database/' + newDatabaseName + '/_all_docs')
       .clickWhenVisible('#faux-header__doc-header-dropdown-toggle')
-      .clickWhenVisible('.faux-header__doc-header-dropdown-itemwrapper a[href*="new_view"]')
+      .clickWhenVisible('div.dropdown-menu a[href*="new_view"]')
       .waitForElementVisible('#new-ddoc', waitTime, false)
       .setValue('#new-ddoc', 'test_design_doc-selenium-bad-reduce')
       .clearValue('input#index-name')
@@ -37,7 +37,7 @@ module.exports = {
       .closeNotifications()
       .clickWhenVisible('.control-toggle-queryoptions', waitTime, false)
       .clickWhenVisible('label[for="qoReduce"]', waitTime, false)
-      .clickWhenVisible('.query-options .btn-secondary', waitTime, false)
+      .clickWhenVisible('#query-options-tray #button-options .btn-cf-secondary', waitTime, false)
       .waitForElementVisible('div.table-view-docs', waitTime, false)
       .waitForAttribute('.table-view-docs td:nth-child(4)', 'title', function (docContents) {
         return (/_sum function requires/).test(docContents);
@@ -56,7 +56,7 @@ module.exports = {
       .url(baseUrl + '/#/database/' + newDatabaseName + '/_design/brokenview/_view/brokenview')
       .clickWhenVisible('.control-toggle-queryoptions', waitTime, false)
       .clickWhenVisible('label[for="qoReduce"]', waitTime, false)
-      .clickWhenVisible('.query-options .btn-secondary', waitTime, false)
+      .clickWhenVisible('#query-options-tray #button-options .btn-cf-secondary', waitTime, false)
       .waitForElementVisible('div.table-view-docs', waitTime, false)
       .waitForAttribute('.table-view-docs td:nth-child(4)', 'title', function (docContents) {
         return (/_sum function requires/).test(docContents);
